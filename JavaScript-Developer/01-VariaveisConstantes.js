592ce8ee632a972009a6a465fd3f1b3ce04da552
Taisso Cout <taissocout@gmail.com>
2026-03-31 10:04:33 -0300

    Create 02-VariaveisOperadores, and modified 01-VariaveisConstantes

--- JavaScript-Developer/01-VariaveisConstantes.js
+++ JavaScript-Developer/01-VariaveisConstantes.js
@@ -18,16 +18,4 @@ console.log(variavel2); // Output: Eu sou uma variável var atualizada
 
 console.log("Olá, mundo!");
 
-// -> Variaveis e tipos de operadores
-// ->  no JS temos 3 tipos de operadores: aritméticos, de comparação e lógicos
-// ->  os operadores aritméticos são: +, -, *, /, %
-
-let num10 = 10;
-let num20 = 20;
-
-console.log("Aqui Estamos Somando", num10 + num20); // logs with automatic spacing // Output: 30 - Aqui estamos usando o operador de adição para somar os valores das variáveis num10 e num20, resultando em 30.
-console.log("Aqui estamos Subtraindo", num10 - num20); // Output: 10 - Aqui estamos usando o operador de subtração para subtrair o valor da variável num20 do valor da variável num10, resultando em -10.
-console.log("Aqui estamos Multiplicando",num10 * num20); // Output: 200 - Aqui estamos usando o operador de multiplicação para multiplicar os valores das variáveis num10 e num20, resultando em 200.
-console.log("Aqui estamos Dividindo",num10 / num20); // Output: 0.5 - Aqui estamos usando o operador de divisão para dividir o valor da variável num10 pelo valor da variável num20, resultando em 0.5.
-console.log("Aqui estamos com o resto",num10 % num20); // Output: 10 - Aqui estamos usando o operador de módulo para obter o resto da divisão do valor da variável num10 pelo valor da variável num20, resultando em 10.
 
