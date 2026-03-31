e7b400178e85e33072b0f56634e829aa54304700
Taisso Cout <taissocout@gmail.com>
2026-03-31 09:54:06 -0300

    Primeiro commit - estrutura inicial do projeto

---
+++ JavaScript-Developer/01-VariaveisConstantes.js
@@ -0,0 +1,19 @@
+// Variáveis e Constantes
+
+// ->  no JS temos 2 formas de declarar variáveis: var e let
+// ->  var é a forma antiga de declarar variáveis, e let é a forma mais moderna e recomendada
+// ->  var tem escopo global ou de função, enquanto let tem escopo de bloco
+
+let vativel = "Eu sou uma variável let";
+console.log(vativel); // Output: Eu sou uma variável
+
+vativel = "Eu sou uma variável let atualizada";
+console.log(vativel); // Output: Eu sou uma variável atualizada
+
+var variavel2 = "Eu sou uma variável var";
+console.log(variavel2); // Output: Eu sou uma variável var
+variavel2 = "Eu sou uma variável var atualizada";
+console.log(variavel2); // Output: Eu sou uma variável var atualizada
+
+
+console.log("Olá, mundo!");
