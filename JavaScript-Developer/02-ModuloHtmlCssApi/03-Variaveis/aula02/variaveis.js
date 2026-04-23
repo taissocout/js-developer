8a87b1e87ff39388f3ea6069eee4463255440337
Taisso Cout <taissocout@gmail.com>
2026-04-23 10:01:26 -0300

    update aula2

--- JavaScript-Developer/02-ModuloHtmlCssApi/03-Variaveis/aula02/variaveis.js
+++ JavaScript-Developer/02-ModuloHtmlCssApi/03-Variaveis/aula02/variaveis.js
@@ -4,3 +4,6 @@ function teste() {
     console.log('Teste');
 }
 
+// Variáveis são espaços na memória para armazenar dados.
+// Para criar uma variável, usamos a palavra-chave var, let ou const, seguida do nome da variável e do valor que queremos atribuir a ela.
+
