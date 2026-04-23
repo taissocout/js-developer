7d89f4b412d973af9ed9a239842b6be8cf919556
Taisso Cout <taissocout@gmail.com>
2026-04-23 10:01:57 -0300

    update aula2

--- JavaScript-Developer/02-ModuloHtmlCssApi/03-Variaveis/aula02/variaveis.js
+++ JavaScript-Developer/02-ModuloHtmlCssApi/03-Variaveis/aula02/variaveis.js
@@ -7,3 +7,7 @@ function teste() {
 // Variáveis são espaços na memória para armazenar dados.
 // Para criar uma variável, usamos a palavra-chave var, let ou const, seguida do nome da variável e do valor que queremos atribuir a ela.
 
+// diferença de var, let e const:
+// var: é a forma mais antiga de declarar variáveis. Ela tem escopo global ou de função, o que pode levar a problemas de escopo e hoisting.
+// let: é a forma mais moderna de declarar variáveis. Ela tem escopo de bloco, o que significa que só existe dentro do bloco onde foi declarada.
+// const: é usada para declarar constantes, ou seja, valores que não podem ser reatribuídos. Ela também tem escopo de bloco.
